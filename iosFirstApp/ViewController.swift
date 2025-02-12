2d548fe52d07e50432f1fe1f9cbbde7b23109da5
1140074 <alejandro.guerrah@elektra.com.mx>
2025-02-11 18:04:56 -0600

    initial commit

--- iosFirstApp/ViewController.swift
+++ iosFirstApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    var unaoDos = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("hello world")
     }
 
-
+    @IBAction func changedClicked(_ sender: Any) {
+        print("change clicked")
+        if unaoDos {
+            imageView.image = UIImage(named: "metallica2")
+            unaoDos = !unaoDos
+        } else {
+            imageView.image = UIImage(named: "metallica1")
+            unaoDos = !unaoDos
+        }
+    }
+    
 }
 
